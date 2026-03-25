2515fad2983cf7af76ca80480c95c0868a1e87df
keerthana <g.keerthana436@outlook.com>
2026-03-24 21:19:14 -0400

    new file:   data/cleaned_data.csv new file:   images/Average tip percentage by time.png new file:   images/Total Sales By Day.png modified:   python/analysis.py

--- python/analysis.py
+++ python/analysis.py
@@ -1,10 +1,13 @@
-
+from pathlib import Path
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+source_file_path = Path('data') / 'tips.csv'
+
 # Load dataset
-df = pd.read_csv("tips.csv")
+df = pd.read_csv(source_file_path)
 
 # Display data
 print(df.head())
@@ -19,7 +22,6 @@ df.rename(columns={
 
 # Check missing values
 print(df.isnull().sum())
-
 # Create new columns
 df['tip_percentage'] = (df['tip_amount'] / df['total_sales']) * 100
 
@@ -28,7 +30,10 @@ df['day'] = df['day'].astype('category')
 df['time'] = df['time'].astype('category')
 
 # Save cleaned data
-df.to_csv("cleaned_data.csv", index=False)
+#df.to_csv("data/cleaned_data.csv", index=False)
+# Display data
+print(df.head())
+print(df.info())
 
 # ======================
 # 📊 Exploratory Analysis
@@ -36,7 +41,17 @@ df.to_csv("cleaned_data.csv", index=False)
 
 # Total sales by day
 sales_by_day = df.groupby('day')['total_sales'].sum()
-print(sales_by_day)
+print("Total Sales by Day:", sales_by_day)
+
+# Total count by day    
+count_by_day = df.groupby('day')['customer_count'].sum()
+print("Total Customer Count by Day:", count_by_day)
+
+# Total sales by day    
+salescount_by_day = df.groupby('day')['total_sales'].count()
+print("Total Customer Count by Day:", salescount_by_day)
+
+
 
 sales_by_day.plot(kind='bar', title='Sales by Day')
 plt.show()
@@ -44,9 +59,9 @@ plt.show()
 # Average tip percentage by time
 tip_by_time = df.groupby('time')['tip_percentage'].mean()
 print(tip_by_time)
-
 tip_by_time.plot(kind='bar', title='Tip % by Time')
 plt.show()
+
 
 # Top 10 highest bills
 top_sales = df.sort_values(by='total_sales', ascending=False).head(10)
